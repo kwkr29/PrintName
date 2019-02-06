4c27f1636a908d36cb3f8c9bdc4651a94cbce25f
kyle <kyle@134.124.110.231>
2019-02-05 18:28:22 -0600

    Added hello git printout

---
+++ src/printname/PrintName.java
@@ -0,0 +1,15 @@
+
+package printname;
+
+public class PrintName {
+
+    public static void main(String[] args) {
+
+        String first_name = 
+        
+        System.out.println("full_name");   
+        
+        
+    }
+    
+}
